601ce967bf01eee0dcd8b8b14bfc0f5805e4333a
Jaroslav Knapek <jaroslav.knapek@mgm-tp.com>
2024-10-17 15:40:07 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-page>
+      <ion-header>
+          <ion-toolbar>
+              <ion-buttons slot="start">
+                  <ion-back-button />
+              </ion-buttons>
+              <ion-title>{{ pageTitle }}</ion-title>
+          </ion-toolbar>
+      </ion-header>
+      <ion-content>
+          <ion-button @click="openModal">Hello</ion-button>
+      </ion-content>
+  </ion-page>
+</template>
+
+<script>
+export default {
+}
+</script>
