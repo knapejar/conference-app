848921479910e25106131f0f9f86b1c59f66c017
Jaroslav Knapek <jaroslav.knapek@mgm-tp.com>
2024-10-22 13:40:40 +0200

    Add vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path'
 import { defineConfig } from 'vite'
 import Components from 'unplugin-vue-components/vite';
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     vue(),
     legacy(),
+    VitePWA({ registerType: 'autoUpdate' }),
     Components({
       resolvers: [IonicResolver()], // Automatically imports Ionic components
     }),
