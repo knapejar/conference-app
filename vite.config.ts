bef0f9b22945a7993746e98d103c106a9f637d9f
Jaroslav Knapek <jaroslav.knapek@mgm-tp.com>
2024-11-08 11:25:52 +0100

    Add pwa config for vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,31 @@ export default defineConfig({
   plugins: [
     vue(),
     legacy(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Ionic PWA',
+        short_name: 'IonicPWA',
+        description: 'An Ionic Vue Progressive Web App',
+        theme_color: '#3880ff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      },
+      workbox: {
+      }
+    }),
     Components({
       resolvers: [IonicResolver()], // Automatically imports Ionic components
     }),
