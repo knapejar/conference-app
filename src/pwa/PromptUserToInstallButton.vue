186004aea35fbf081b8091233f5c817173066f2a
Jaroslav Knapek <jaroslav.knapek@mgm-tp.com>
2024-11-08 11:47:00 +0100

    Modify settings to make the PromptUserToInstall button work

--- src/pwa/PromptUserToInstallButton.vue
+++ src/pwa/PromptUserToInstallButton.vue
@@ -1,49 +1,48 @@
 <template>
-    <ion-button v-if="showInstallButton" @click="handleInstall">
-      Install App
-    </ion-button>
+	<ion-button v-if="showInstallButton" @click="handleInstall">
+	  Install App
+	</ion-button>
   </template>
   
   <script lang="ts">
   import { ref, onMounted } from 'vue'
   
   export default {
-    setup() {
-      const showInstallButton = ref(false)
-      const deferredPrompt = ref(null)
+	setup() {
+	  const showInstallButton = ref(false)
+	  let deferredPrompt: BeforeInstallPromptEvent | null = null
   
-      const handleInstall = async () => {
-        if (deferredPrompt.value) {
-          deferredPrompt.value.prompt()
-          const { outcome } = await deferredPrompt.value.userChoice
-          console.log(`User response to the install prompt: ${outcome}`)
-          deferredPrompt.value = null
-        }
-      }
+	  const handleInstall = async () => {
+		if (deferredPrompt) {
+		  deferredPrompt.prompt()
+		  const { outcome } = await deferredPrompt.userChoice
+		  console.log(`User response to the install prompt: ${outcome}`)
+		  deferredPrompt = null
+		}
+	  }
   
-      onMounted(() => {
-        window.addEventListener('beforeinstallprompt', (e) => {
-          e.preventDefault()
-          deferredPrompt.value = e
-          showInstallButton.value = true
-        })
+	  onMounted(() => {
+		window.addEventListener('beforeinstallprompt', (e) => {
+		  e.preventDefault()
+		  deferredPrompt = e as BeforeInstallPromptEvent
+		  showInstallButton.value = true
+		})
   
-        window.addEventListener('appinstalled', () => {
-          showInstallButton.value = false
-        })
-      })
+		window.addEventListener('appinstalled', () => {
+		  showInstallButton.value = false
+		})
+	  })
   
-      return { showInstallButton, handleInstall }
-    },
+	  return { showInstallButton, handleInstall }
+	},
   }
   </script>
   
   <style scoped>
   ion-button {
-    position: fixed;
-    top: 10px;
-    right: 10px;
-    z-index: 1000;
+	position: fixed;
+	top: 10px;
+	right: 10px;
+	z-index: 1000;
   }
   </style>
-  
